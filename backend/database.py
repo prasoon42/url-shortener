8ad2676cc540af7c9e06ff7af1f7369aa6c94c5f
Prasoon Mishra <146659326+prasoon42@users.noreply.github.com>
2026-07-16 09:17:32 +0530

    Clean up database.py
    Removed unnecessary comments and documentation.

--- backend/database.py
+++ backend/database.py
@@ -1,25 +1,16 @@
-"""
-database.py - MongoDB Connection Setup
-"""
 
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from pathlib import Path
 import os
 
-# Load .env explicitly
 env_path = Path(__file__).parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 MONGO_URI = os.getenv("MONGO_URI")
 
-
-
-# Connect to Atlas
 client = MongoClient(MONGO_URI)
 
-# Database
 db = client["url_shortener"]
 
-# Collection
 collection = db["urls"]
